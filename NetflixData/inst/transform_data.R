941c3f042fdbc9a5560dc064f22cd273a53cc1b2
Ismah <57876828+ismahahmed@users.noreply.github.com>
2021-11-11 22:03:49 -0500

    Add files via upload

---
+++ NetflixData/inst/transform_data.R
@@ -0,0 +1,6 @@
+
+#country_data=read.csv("country_data.csv",sep=",",stringsAsFactors = FALSE)
+#save(country_data,file="country_data.rda")
+
+#country_listedin=read.csv("country_listedin.csv",sep=",",stringsAsFactors = FALSE)
+#save(country_listedin,file="country_listedin.rda")
